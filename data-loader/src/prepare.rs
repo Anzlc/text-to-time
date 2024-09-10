294a8a316373e7f8cc2c4e02dcb25a97214afbe4
Anzlc <107500985+Anzlc@users.noreply.github.com>
2024-09-10 15:35:15 +0200

    Implemented generatetrainingdata

--- data-loader/src/prepare.rs
+++ data-loader/src/prepare.rs
@@ -1,17 +1,28 @@
+use std::collections::HashMap;
+
 use crate::TimeData;
 
-pub fn generate_training_data(data: &[TimeData]) -> (Vec<[f64; 64]>, Vec<[f64; 4]>) {
+pub fn generate_training_data(data: &[TimeData]) -> (Vec<[f64; 64]>, Vec<[f64; 2]>) {
     let mut inputs: Vec<[f64; 64]> = Vec::new();
-    let mut outputsVec: Vec<[f64; 4]> = Vec::new();
+    let mut outputs: Vec<[f64; 2]> = Vec::new();
 
-    for time in data {
+    let mut character_map: HashMap<char, f64> = HashMap::new();
+
+    for (i, time) in data.into_iter().enumerate() {
         let mut natural_input: [f64; 64] = [0.0; 64];
+        let mut formatted_output: [f64; 2] = [0.0; 2];
         if time.natural_time.len() > 64 {
             panic!("Input \"{}\" exceeded 64 characters!", time.natural_time);
         }
-        for c in time.natural_time.chars() {
+        for (j, c) in time.natural_time.chars().enumerate() {
+            if !character_map.contains_key(&c) {
+                character_map.insert(c, character_map.len() as f64);
+            }
+            natural_input[j] = *character_map.get(&c).unwrap();
         }
+        inputs.push(natural_input);
+        outputs.push(time.get_parsed_time());
     }
 
-    (vec![], vec![])
+    (inputs, outputs)
 }
